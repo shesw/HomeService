5281740052a4e4844cf28793fc4dbdfb6eaa6ecb
shesw <anjicsl14@gmail.com>
2023-01-01 18:12:34 +0800

    [fun]支持路径设置

--- services/home_service_main.js
+++ services/home_service_main.js
@@ -12,43 +12,50 @@ app.get('/home', (req, res) => {
 })
 
 app.post("/upload", function (req, res) {
-    /* 生成multiparty对象，并配置上传目标路径 */
-    let form = new multiparty.Form();
-    // 设置编码 
-    form.encoding = 'utf-8';
-    // 设置文件存储路径，以当前编辑的文件为相对路径
-    form.uploadDir = FILE_SAVE_DIR;
-    // 设置文件大小限制
-    // form.maxFilesSize = 1 * 1024 * 1024;
-    form.parse(req)
+    try {
+        /* 生成multiparty对象，并配置上传目标路径 */
+        let form = new multiparty.Form();
+        // 设置编码 
+        form.encoding = 'utf-8';
+        // 设置文件存储路径，以当前编辑的文件为相对路径
+        form.uploadDir = FILE_SAVE_DIR;
+        // 设置文件大小限制
+        // form.maxFilesSize = 1 * 1024 * 1024;
+        form.parse(req)
 
-    var fileHandler = FileHander()
+        var fileHandler = FileHander()
 
-    form.on('field', (name, value) => { // 接收到数据参数时，触发field事件
-        console.log('on field')
-        console.log(name, value)
-        if (name == 'saveDir') {
-            fileHandler.saveDir = value
-            if (fileHandler.saveDir == null || fileHandler.saveDir == '' || fileHandler.saveDir == undefined) {
-                fileHandler.saveDir = '/'
+        form.on('field', (name, value) => { // 接收到数据参数时，触发field事件
+            console.log('on field')
+            console.log(name, value)
+            if (name == 'saveDir') {
+                fileHandler.saveDir = value
+                if (fileHandler.saveDir == null || fileHandler.saveDir == '' || fileHandler.saveDir == undefined) {
+                    fileHandler.saveDir = '/'
+                }
+                fileHandler.handle()
             }
+        })
+
+        form.on('file', (name, file, ...rest) => { // 接收到文件参数时，触发file事件
+            console.log('on file')
+            console.log(name, file)
+
+            fileHandler.originalFilename = file.originalFilename
+            fileHandler.filePath = file.path
             fileHandler.handle()
-        }
-    })
+            res.end()
+        })
 
-    form.on('file', (name, file, ...rest) => { // 接收到文件参数时，触发file事件
-        console.log('on file')
-        console.log(name, file)
-
-        fileHandler.originalFilename = file.originalFilename
-        fileHandler.filePath = file.path
-        fileHandler.handle()
+        form.on('close', () => { // 表单数据解析完成，触发close事件
+            console.log('表单数据解析完成')
+        })
+    } catch (e) {
+        console.log('upload fail, e=' + e)
+        res.send(e)
         res.end()
-    })
+    }
 
-    form.on('close', () => { // 表单数据解析完成，触发close事件
-        console.log('表单数据解析完成')
-    })
 });
 
 app.listen(7777, () => {
